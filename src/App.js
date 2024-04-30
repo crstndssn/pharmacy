0074d5b35f58fdd9650d7a99bc9400123964e481
Cristian Dussán <cristian@sltwtr.com>
2024-04-29 20:47:20 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+// components
+import Navigation from './components/Navigation'
+// import Footer from './components/Footer'
+
+// pages
+import Home from './pages/Home'
+import Api from './pages/Api'
+
 function App() {
   return (
-    <div className='text-3xl uppercase'>
-			<p>pharmacy</p>
-		</div>
+    <>
+			<Navigation/>
+			<Api />
+			<Home />
+		</>
   );
 }
 
---
+++ src/pages/Api.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const options = {
+	method: 'GET',
+	url: 'https://drug-info-and-price-history.p.rapidapi.com/1/druginfo',
+	params: { drug: 'advil' },
+	headers: {
+		'X-RapidAPI-Key': '9722fb91c5mshfb100fa9dd27dc2p1192c1jsn0d48ab67693d',
+		'X-RapidAPI-Host': 'drug-info-and-price-history.p.rapidapi.com'
+	}
+};
+
+const Api = () => {
+	const [drugs, setDrugs] = useState();
+
+	useEffect(() => {
+
+			axios.request(options).then(function (response) {
+				console.log(response.data);
+				debugger
+			}).catch(function (error) {
+				console.error(error);
+			})
+
+
+	}, []); // Empty dependency array to run once on component mount
+
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+export default Api;
