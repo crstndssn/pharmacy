f853e53b351d75c96521d19c52e879dad1f6f056
Cristian Dussán <cristian@sltwtr.com>
2024-05-07 09:00:41 -0500

    feature: update routers and home

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
 	return (
-		<div>
-			<h2>
-				pharmacy
-			</h2>
-			<a>user</a>
+		<div className="contianer mx-auto w-full flex justify-between items-center">
+			<div className="w-1/2">
+					<h2 className="font-bold text-2xl">pharmacy</h2>
+			</div>
+			<div className="w-1/2 flex justify-between">
+				<Link exact to='/home'>Inicio</Link>
+				<Link to='/product'>Porductos</Link>
+				<Link to='/contact'>Contact</Link> 
+			</div>
 		</div>
 	)
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,34 +4,50 @@ import header from '../images/header.jpg'
 
 const Home = () => {
 	return (
-		<div className="container mx-auto">
-			<div className="w-full">
-				<img className="h-1/2" src={header} />
+		<div className="">
+			<div className="w-full h-56 overflow-hidden">
+				<img className="w-full h-auto" src={header} />
 			</div>
 
-			<div className="grid grid-cols-3">
-				<div className="border border-green-600">
+			<div className="container mx-auto grid grid-cols-4 py-12">
+				<div className="flex justify-center items-center border border-green-600 rounded-full w-36 h-36">
 					<h2>Ofertas</h2>
 				</div>
-				<div>
+				<div className="flex justify-center items-center border border-green-600 rounded-full w-36 h-36">
 					<h2>Medicamentos</h2>
 				</div>
-				<div>
+				<div className="flex justify-center items-center border border-green-600 rounded-full w-36 h-36">
 					<h2>Formulados</h2>
 				</div>
-				<div>
+				<div className="flex justify-center items-center border border-green-600 rounded-full w-36 h-36">
 					<h2>Formulados</h2>
 				</div>
 			</div>
 
 
-			<h2></h2>
+			<h2>Ofertas</h2>
 			<div>
 				<div className="">
 					<img src="https://beta1.cruzverde.com.co/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_Colombia/default/dwe1202ec7/images/large/550615_1_LIMPIADOR_FACIAL_DIARIO_CETAPHIL_PIEL_MIXTA_A_GRASA_Y_SENSIBLE_FCO_X_473ML%20(2).jpg?sw=295&sh=295" />
 					<h2>cremas</h2>
 				</div>
 				<div className="">
+					<img src="https://beta1.cruzverde.com.co/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_Colombia/default/dwe1202ec7/images/large/550615_1_LIMPIADOR_FACIAL_DIARIO_CETAPHIL_PIEL_MIXTA_A_GRASA_Y_SENSIBLE_FCO_X_473ML%20(2).jpg?sw=295&sh=295" />
+					<h2>cremas</h2>
+				</div>
+			</div>
+
+			<h2>Medicamentos</h2>
+			<div>
+				<div>
+					<img src="https://beta1.cruzverde.com.co/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_Colombia/default/dwe1202ec7/images/large/550615_1_LIMPIADOR_FACIAL_DIARIO_CETAPHIL_PIEL_MIXTA_A_GRASA_Y_SENSIBLE_FCO_X_473ML%20(2).jpg?sw=295&sh=295" />
+					<h2>cremas</h2>
+				</div>
+				<div>
+					<img src="https://beta1.cruzverde.com.co/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_Colombia/default/dwe1202ec7/images/large/550615_1_LIMPIADOR_FACIAL_DIARIO_CETAPHIL_PIEL_MIXTA_A_GRASA_Y_SENSIBLE_FCO_X_473ML%20(2).jpg?sw=295&sh=295" />
+					<h2>cremas</h2>
+				</div>
+				<div>
 					<img src="https://beta1.cruzverde.com.co/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_Colombia/default/dwe1202ec7/images/large/550615_1_LIMPIADOR_FACIAL_DIARIO_CETAPHIL_PIEL_MIXTA_A_GRASA_Y_SENSIBLE_FCO_X_473ML%20(2).jpg?sw=295&sh=295" />
 					<h2>cremas</h2>
 				</div>
