5fd3e668a88a7eda77e29a5ce3f0c85d63465494
Cristian Dussán <cristian@sltwtr.com>
2024-05-09 09:42:30 -0500

    feature: home updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,9 +27,12 @@ const Home = () => {
 
 			<h2>Ofertas</h2>
 			<div>
-				<div className="">
+				<div className="shadow-xl">
 					<img src="https://beta1.cruzverde.com.co/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_Colombia/default/dwe1202ec7/images/large/550615_1_LIMPIADOR_FACIAL_DIARIO_CETAPHIL_PIEL_MIXTA_A_GRASA_Y_SENSIBLE_FCO_X_473ML%20(2).jpg?sw=295&sh=295" />
-					<h2>cremas</h2>
+					<div className="flex justify-between">
+						<h2>cremas</h2>
+						<p className="bg-green-700 text-white">$55.0000</p>
+					</div>
 				</div>
 				<div className="">
 					<img src="https://beta1.cruzverde.com.co/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_Colombia/default/dwe1202ec7/images/large/550615_1_LIMPIADOR_FACIAL_DIARIO_CETAPHIL_PIEL_MIXTA_A_GRASA_Y_SENSIBLE_FCO_X_473ML%20(2).jpg?sw=295&sh=295" />
