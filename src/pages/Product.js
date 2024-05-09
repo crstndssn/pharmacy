7df8888aa4228a02ce02d90fe99f99be11edd5d4
Cristian Dussán <cristian@sltwtr.com>
2024-05-09 18:38:45 -0500

    feature: add router

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const Product = () => {
 	return (
 		<div className="container mx-auto ">
@@ -12,7 +11,7 @@ const Product = () => {
 				<div className="w-1/2">
 					<h1 className="font-bold text-3xl mb-10">Esta es mi ficha de producto</h1>
 					<p className="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-									<a className="bg-green-700 p-4 text-white rounded-xl border border-green-700 hover:bg-white hover:text-green-700">comprar</a>
+					<a className="bg-green-700 p-4 text-white rounded-xl border border-green-700 hover:bg-white hover:text-green-700">comprar</a>
 				</div>
 			</div>
 		</div>
